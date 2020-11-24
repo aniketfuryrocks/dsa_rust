23325e819f46fdf9d04765ad185308fee9c84107
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-11-24 22:54:27 +0530

    unsafe fn instead of blocks

--- src/data_structs/stacks.rs
+++ src/data_structs/stacks.rs
@@ -9,14 +9,12 @@ pub struct Stack<T> {
 }
 
 impl<T> Stack<T> {
-    pub fn new(size: usize) -> Stack<T> {
+    pub unsafe fn new(size: usize) -> Stack<T> {
         let mut stack = Stack {
             arr: Vec::with_capacity(size),
             len: 0,
         };
-        unsafe {
-            stack.arr.set_len(size);
-        }
+        stack.arr.set_len(size);
         stack
     }
 
@@ -30,21 +28,19 @@ impl<T> Stack<T> {
             Ok(())
         }
     }
-    pub fn pop(&mut self) -> Result<T, &'static str> {
+    pub unsafe fn pop(&mut self) -> Result<T, &'static str> {
         if self.len == 0 {
             Err("Stack Underflow")
         } else {
             self.len -= 1;
-            // though reference can be returned but a naive stack structure returns the value itself
-            unsafe {
-                Ok(ptr::read(self.arr.as_ptr().add(self.len)))
-            }
+            // though a reference can be returned but a naive stack structure returns the value itself
+            Ok(ptr::read(self.arr.as_ptr().add(self.len)))
         }
     }
 }
 
 #[test]
-fn stack_test() {
+unsafe fn stack_test() {
     let mut stack = Stack::new(20);
     stack.push(1).unwrap();
     stack.push(2).unwrap();
